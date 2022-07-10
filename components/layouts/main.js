f76bc1d5d225d4c773cdfa237c42f98f95b0c92f
Peoples Champion <95397105+ThePeoplesChamp@users.noreply.github.com>
2022-07-10 18:13:11 +0930

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,14 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
+        <meta name="description" content="Jacobs Homepage" />
         <meta name="author" content="Jacob Clement" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Jacob Clement" />
+        <meta name="og:title" content="Jacob Clement" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" href="apple-touch-icon.png" />
         <title>Jacob Clement - Homepage</title>
       </Head>
 
